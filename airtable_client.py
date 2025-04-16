5639c151201f73c13ff835ae18fefe70f2d42a0c
Bubi-mubi <bube.office@gmail.com>
2025-04-16 22:12:41 +0300

    Update airtable_client.py

--- airtable_client.py
+++ airtable_client.py
@@ -1,1 +1,21 @@
+import requests
+import os
+
+class AirtableClient:
+    def __init__(self):
+        self.token = os.getenv("AIRTABLE_PAT")
+        self.base_id = os.getenv("AIRTABLE_BASE_ID")
+        self.table_name = os.getenv("AIRTABLE_TABLE_NAME")
+        self.endpoint = f"https://api.airtable.com/v0/{self.base_id}/{self.table_name}"
+        self.headers = {
+            "Authorization": f"Bearer {self.token}",
+            "Content-Type": "application/json"
+        }
+
+    def add_record(self, fields: dict):
+        data = {
+            "fields": fields
+        }
+        response = requests.post(self.endpoint, headers=self.headers, json=data)
+        return response.json()
 
