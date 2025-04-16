b2a440db4ceff55deb7e281aa49af3e2c4d73413
Bubi-mubi <bube.office@gmail.com>
2025-04-16 22:05:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+from telethon import TelegramClient, events
+from airtable_client import AirtableClient
+from dotenv import load_dotenv
+import os
+import datetime
+
+load_dotenv()
+
+bot_token = os.getenv("BOT_TOKEN")
+
+# Стартираме само с bot_token – без API_ID и API_HASH
+client = TelegramClient('bot_session', api_id=None, api_hash=None).start(bot_token=bot_token)
+airtable = AirtableClient()
 
